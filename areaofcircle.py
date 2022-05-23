5e8da80f77e3889fdef977c405beef2f28c81ad7
Sahil Naik <104933845+SahilN27@users.noreply.github.com>
2022-05-23 08:49:55 +0530

    Update areaofcircle.py

--- areaofcircle.py
+++ areaofcircle.py
@@ -2,3 +2,6 @@ PI = 3.14
 r = float(input("The radius of the circle : "))
 Area = PI*r*r
 print("Area of a circle = %.2f" %Area)
+
+Filename = input("The FileName :")
+print("The extension of the file is :'python'")
