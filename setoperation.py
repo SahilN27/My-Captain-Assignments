c3a53c9fbcae8bc2c3c61e0cf4a56a3b26e386d1
Sahil Naik <104933845+SahilN27@users.noreply.github.com>
2022-07-28 21:35:40 +0530

    Add files via upload

---
+++ setoperation.py
@@ -0,0 +1,10 @@
+E = {0,1,2,3}
+N = {4,5,6,8}
+
+print("Union of E and N is", E | N)
+
+print("Intersection of E and N is", E & N)
+
+print("Difference of E and N is", E - N)
+
+print("Symmetric differnce of E and N is", E ^ N)
